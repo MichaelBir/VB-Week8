b714d8420bc4a024d07936d1415a763adb3114c9
michaelbir <mikux22@gmail.com>
2023-05-11 18:21:06 +0300

    first commit

---
+++ Read_Data_from_file/Program.cs
@@ -0,0 +1,22 @@
+﻿string folderPath = @"C:\data\";
+string heroFile = "heros.txt";
+string villainFile = "villains.txt";
+
+string[] heros = FileShare.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = FileShare.ReaAllLines(Path.Combine(folderPath, villainFile));
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Täna {hero} võitleb {heroWeapon} et päästa maailm! ");
+
+string villain = GetRandomValueFromArray(villanis);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Täna {villain} koos {villainWeapon} püüab maailma vallutada");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
---
+++ Week_8/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] heroes = { "Harry potter", "Lara Croft", "Siil udus", "Onu Reimus" };
+string[] villanis = { "Kana", "Vares", "Kull", "Kukk" };
+string[] weapon = { "hark", "kõbla", "labidas", "reha" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Täna {hero} võitleb {heroWeapon} et päästa maailm! ");
+
+string villain = GetRandomValueFromArray(villanis);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Täna {villain} koos {villainWeapon} püüab maailma vallutada");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
